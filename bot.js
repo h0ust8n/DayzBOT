819d948743d6c307be0d7ffc097c4626edfa4b6b
h0ust8n <103719091+h0ust8n@users.noreply.github.com>
2022-04-15 07:07:37 +0300

    TEST

---
+++ bot.js
@@ -0,0 +1,17 @@
+const {Client} = require("discord.js");
+const bot = new Client();
+const gamedig = require('gamedig');
+async function online() {
+	gamedig.query({type: 'dayz', host: '212.22.93.93', port: '2342'
+		}).then((state) => {
+			bot.user.setActivity(`🎮 Онлайн: ${state.raw.numplayers}/${state.maxplayers}`, {type: 0});
+		}).catch((error) => {
+			bot.user.setActivity(`🎮 Сервер offline`, {type: 0});
+		});
+}
+bot.login(process.env.BOT_TOKEN)
+bot.on('ready', async () => {
+	console.log('BOT UP')
+	online()
+	setInterval(online, 10000)
+});
